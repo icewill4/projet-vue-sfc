67c1465ae6b2f5b23f86972c7e4530b4afdc3ea7
icewill <alexis.bertin01@gmail.com>
2023-01-05 17:14:45 +0100

    page-equipe2

--- src/views/EquipeView.vue
+++ src/views/EquipeView.vue
@@ -1,17 +1,63 @@
 <template>
-    <div class="home">
-      <img alt="Vue logo" src="../assets/logo.png">
+    <div>
+      <h1>My Pokemon Team</h1>
+      <ul>
+        <li v-for="pokemon in pokemons" :key="pokemon.id">
+          {{ pokemon.name }}
+          <button @click="editPokemon(pokemon)">Edit</button>
+          <button @click="deletePokemon(pokemon)">Delete</button>
+        </li>
+      </ul>
+      <h2 v-if="pokemons.length < 6">Add Pokemon</h2>
+      <form v-if="pokemons.length < 6" @submit.prevent="addPokemon">
+        <label>
+          Name:
+          <input v-model="newPokemon.name" type="text" />
+        </label>
+        <button type="submit">Add</button>
+      </form>
     </div>
   </template>
   
   <script>
-
-  
   export default {
-    name: 'EquipeView',
-    components: {
+    name: "PokemonList",
+    data() {
+      return {
+        newPokemon: { name: "" },
+        pokemons: [
+          { id: 1, name: "Pikachu" },
+          { id: 2, name: "Charmander" },
+          { id: 3, name: "Squirtle" },
+        ],
+      };
+    },
+    methods: {
+        addPokemon() {
+            if (this.newPokemon.name === "") {
+                // Display an error message or return early if the input is empty
+                return;
+            }
+            // Generate a new ID for the pokemon
+            this.newPokemon.id = Math.max(...this.pokemons.map((p) => p.id)) + 1;
 
-    }
-  }
+            // Add the new pokemon to the list
+            this.pokemons.push(this.newPokemon);
+
+            // Reset the newPokemon object
+            this.newPokemon = { name: "" };
+            },
+      editPokemon(pokemon) {
+        // Edit the pokemon here
+      },
+      deletePokemon(pokemon) {
+        // Find the index of the pokemon in the list
+        const index = this.pokemons.indexOf(pokemon);
+  
+        // Remove the pokemon from the list
+        this.pokemons.splice(index, 1);
+      },
+    },
+  };
   </script>
   
